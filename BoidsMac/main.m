999f499b4a4fdff05f359473cc3831d0d86659d8
iD Student <Student@dhcp-200-185.cee.utexas.edu>
2014-07-23 09:10:27 -0500

    Initial Commit

---
+++ BoidsMac/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BoidsMac
+//
+//  Created by iD Student on 7/23/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
